3fe76bb59d50cfb5694a5b33fded8ebbe8e4b689
Yaswanth <yaswanthraj671@gmail.com>
2018-03-13 10:55:55 +0530

    Added Process abd List to admin dashboard

--- trackprogress/tracking_system/admin.py
+++ trackprogress/tracking_system/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Student
+from .models import Student, Process, List
 # Register your models here.
 admin.site.register(Student)
+admin.site.register(Process)
+admin.site.register(List)
