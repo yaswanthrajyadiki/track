431a1f553c90b4432c866b187f748ae371844fe4
Yaswanth <yaswanthraj671@gmail.com>
2018-03-13 10:44:18 +0530

    Added localhost in ALLOWED_HOSTS list

--- trackprogress/track_progress/settings.py
+++ trackprogress/track_progress/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '12tq+#&b81v@pwb&j_gf451s@m%bgrk+%-1qkymve&^@wo&fxh'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 # Application definition
 
